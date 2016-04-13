2cae0f54576dbad7d2e78c6c9616d2f42cd0f043
Guesmi Hachem <guesmi.hachem@gmail.com>
2016-04-13 12:30:30 +0200

    init :+1:

---
+++ src/projet1/Projet1.java
@@ -0,0 +1,12 @@
+
+package projet1;
+
+public class Projet1 
+{
+    public static void main(String[] args) 
+    {
+       //       -Djava.library.path="C:\opencv\build\java\x64" 
+        
+    }
+    
+}
